8f4c6236c8f60f1cf3cedec3565b65dbb4c05d16
Youssef HIMMI <yhimmi1@gmail.com>
2025-06-01 22:46:57 +0200

    feat(users): use posts array and render UserCard components dynamically

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -4,17 +4,17 @@ import UserCard from "@/components/common/UserCard";
 import { UserProps } from "@/interfaces";
 
 interface UsersPageProps {
-  users: UserProps[];
+  posts: UserProps[];
 }
 
-const Users: React.FC<UsersPageProps> = ({ users }) => {
+const Users: React.FC<UsersPageProps> = ({ posts }) => {
   return (
     <div className="flex flex-col h-screen">
       <Header />
       <main className="p-4">
         <h1 className="text-2xl font-semibold mb-4">Users Page</h1>
         <div className="space-y-4">
-          {users.map((user) => (
+          {posts.map((user) => (
             <UserCard
               key={user.username}
               username={user.username}
@@ -32,8 +32,7 @@ export async function getStaticProps() {
   const response = await fetch("https://jsonplaceholder.typicode.com/users");
   const usersData = await response.json();
 
-  // Map the API user data to the UserProps interface
-  const users: UserProps[] = usersData.map((user: any) => ({
+  const posts: UserProps[] = usersData.map((user: any) => ({
     username: user.username,
     street: user.address.street,
     catchPhrase: user.company.catchPhrase,
@@ -41,7 +40,7 @@ export async function getStaticProps() {
 
   return {
     props: {
-      users,
+      posts,
     },
   };
 }
